b6e7768dc6d673a2790f246e6ab8db89a63897f7
Destroyer WOT <vladimirleviy2005@gmail.com>
2024-11-15 00:37:10 +0300

    6 Задания

---
+++ 6 lab/2 Task.php
@@ -0,0 +1,14 @@
+<?php
+function expanded_form(int $n): string {
+    $result = [];
+    $str = (string)$n;
+    $len = strlen($str);
+
+    for ($i = 0; $i < $len; $i++) {
+        if ($str[$i] != '0') {
+            $result[] = $str[$i] . str_repeat('0', $len - $i - 1);
+        }
+    }
+
+    return implode(' + ', $result);
+}
