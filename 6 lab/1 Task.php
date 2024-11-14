c4cef2085136a9347971eeb0d9de229c20b5adae
Destroyer WOT <vladimirleviy2005@gmail.com>
2024-11-15 00:36:20 +0300

    6 Задания

---
+++ 6 lab/1 Task.php
@@ -0,0 +1,13 @@
+<?php
+function digital_root($number): int
+{
+    while ($number >= 10) {
+        $sum = 0;
+        while ($number > 0) {
+            $sum += $number % 10;
+            $number = intdiv($number,10);  
+        }
+        $number = $sum;  
+    }
+    return $number;
+}
